7d91e2d05383989d2580181f7a81f6b54832abfc
ThomasRevord <94570592+ThomasRevord@users.noreply.github.com>
2023-03-02 19:33:03 -0600

    double click added

--- IM388_Prototype_3/Assets/Scripts/PhotoBehaviour.cs
+++ IM388_Prototype_3/Assets/Scripts/PhotoBehaviour.cs
@@ -7,11 +7,13 @@ public class PhotoBehaviour : MonoBehaviour
 {
     //scene to go to
     public string goTo;
+    //bool to check double click
+    public bool dc;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        dc = false;
     }
 
     // Update is called once per frame
@@ -23,11 +25,24 @@ public class PhotoBehaviour : MonoBehaviour
     //when mouse is over photo
     private void OnMouseOver()
     {
-        //if left mouse button is clicked
-        if (Input.GetMouseButtonDown(0))
-        { 
+        //if left mouse button is clicked and dc is true
+        if (Input.GetMouseButtonDown(0) && dc == true)
+        {
             //go to scene
             SceneManager.LoadScene(goTo);
         }
+        else if(Input.GetMouseButtonDown(0))
+        {
+            //dc is true, next click will go to scene
+            dc = true;
+            Debug.Log("DC: " + dc);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        //reset dc when mouse exits
+        dc = false;
+        Debug.Log("DC: " + dc);
     }
 }
