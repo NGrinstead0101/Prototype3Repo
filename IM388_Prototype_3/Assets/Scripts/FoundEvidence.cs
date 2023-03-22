5742ac962e60ffd7967a22bceb65fd32680059ae
NGrinstead0101 <ngrinstead@mail.bradley.edu>
2023-03-22 11:51:29 -0500

    Photo Positions Persist

--- IM388_Prototype_3/Assets/Scripts/FoundEvidence.cs
+++ IM388_Prototype_3/Assets/Scripts/FoundEvidence.cs
@@ -7,6 +7,7 @@ public static class FoundEvidence
     public static bool[] foundList = new bool[20];
     public static bool[] photosClicked = new bool[4];
     public static Vector2[] notePositions = new Vector2[20];
+    public static Vector2[] photoPositions = new Vector2[4];
 
     // bool for shuffling?
 }
